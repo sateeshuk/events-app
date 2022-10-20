f88ca1b11f20a517cf15d0e2f1445e2cbf05a5aa
sateeshuk <sateeshuk@gmail.com>
2022-10-20 05:50:11 +0000

    app config fix

--- api-server/src/app.config.js
+++ api-server/src/app.config.js
@@ -4,7 +4,7 @@
  * DO NOT store state here - must be part of a stateless service
  */
 class AppConfig {
-    team = 'Your Team Name';
+    team = 'Group 1 Sai Ram';
     version = '1.0.0';
 }
 module.exports = AppConfig;
